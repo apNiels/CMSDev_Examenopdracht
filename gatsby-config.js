65f4b2c783e846c60ba02abb6d29b916b295675c
NielsAP <s117923@ap.be>
2021-01-23 14:31:17 +0100

    Added gatsby-source-graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Examenopdracht`,
+    description: `Voor deze examenopdracht toon ik wat films...`,
+    author: `Niels Van de Poel`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,6 +26,14 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: "gatsby-source-graphql",      
+      options: {      
+        typeName: "WPGraphql",      
+        fieldName: "wpcontent",      
+        url: "http://examenopdracht.local/graphql",   
+      },      
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
